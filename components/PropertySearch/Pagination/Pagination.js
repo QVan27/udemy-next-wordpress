fc4ee712b84ad208368c1609f3e9990f4743bd03
Quentin Vannarath <qvannarath@gmail.com>
2024-02-07 14:44:06 +0100

    ✨[PAGINATION] Handle pagination in search API

--- components/PropertySearch/Pagination/Pagination.js
+++ components/PropertySearch/Pagination/Pagination.js
@@ -1,9 +1,9 @@
-export const Pagination = ({ totalPages }) => {
+export const Pagination = ({ totalPages, onPageClick }) => {
   console.log(totalPages)
   return (
     <div className='max-w-5xl mx-auto mb-10 flex justify-center gap-2'>
-      {Array.from({ length: totalPages}).map((_, i) => (
-        <div key={i} className='btn'>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <div key={i} className='btn' onClick={() => { onPageClick(i + 1) }}>
           {i + 1}
         </div>
       ))}
