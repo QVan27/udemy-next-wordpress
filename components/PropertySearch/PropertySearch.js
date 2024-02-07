b5af2f099362ef7c28cdaeabc542eba981c9a213
Quentin Vannarath <qvannarath@gmail.com>
2024-02-07 14:10:13 +0100

    ✨[PROPERTY SEARCH] Rendering property search results

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,18 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Results } from './Results';
 
 export const PropertySearch = () => {
+  const [properties, setProperties] = useState([])
+
   useEffect(() => {
     const search = async () => {
       const response = await fetch('/api/search')
       const data = await response.json()
 
       console.log(data)
+
+      setProperties(data.properties)
     }
 
     search()
   }, [])
 
   return (
-    <div>Property Search</div>
+    <div>
+      <Results properties={properties} />
+    </div>
   )
 }
