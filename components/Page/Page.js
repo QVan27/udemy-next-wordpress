7bffae76a6b1eef3c2008d1303d350ca6f045469
Quentin Vannarath <qvannarath@gmail.com>
2024-02-12 12:43:32 +0100

    ✨[LENIS] Add SmoothScrolling component and dependencies

--- components/Page/Page.js
+++ components/Page/Page.js
@@ -2,21 +2,25 @@ import { BlockRenderer } from 'components/BlockRenderer';
 import { MainMenu } from 'components/MainMenu';
 import { PageWrapper } from 'context/page';
 import Head from 'next/head';
+import SmoothScrolling from "components/SmoothScrolling";
 
 export const Page = (props) => {
   console.log('PAGE PROPS: ', props);
   return (
-    <PageWrapper value={{ title: props.title, featuredImage: props.featuredImage }}>
-      <Head>
-        <title>{props.seo.title}</title>
-        <meta name="description" content={props.seo.metaDesc} />
-      </Head>
-      <MainMenu
-        items={props.mainMenuItems}
-        callToActionLabel={props.callToActionLabel}
-        callToActionDestination={props.callToActionDestination}
-      />
-      <BlockRenderer blocks={props.blocks} />
-    </PageWrapper>
+    <SmoothScrolling>
+      <PageWrapper value={{ title: props.title, featuredImage: props.featuredImage }}>
+        <Head>
+          <title>{props.seo.title}</title>
+          <meta name="description" content={props.seo.metaDesc} />
+        </Head>
+        <MainMenu
+          items={props.mainMenuItems}
+          callToActionLabel={props.callToActionLabel}
+          callToActionDestination={props.callToActionDestination}
+        />
+
+        <BlockRenderer blocks={props.blocks} />
+      </PageWrapper>
+    </ SmoothScrolling>
   )
 }
