79462cb9a303f683bf178b096791efe5ef98a349
Quentin Vannarath <qvannarath@gmail.com>
2024-02-12 10:12:41 +0100

    ⚡️ [CACHE] Next cache

--- pages/api/revalidate.js
+++
@@ -1,9 +0,0 @@
-import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server";
-
-async function handler(request) {
-  revalidatePath("/", "layout");
-  return NextResponse.json({});
-}
-
-export { handler as POST, handler as GET };
